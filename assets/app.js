eb4f050e030f9acda2158d4afd3e51a4466b7322
Tegar Deyustian <tegardm@gmail.com>
2021-06-30 16:16:55 +0700

    menyelesaikan content main...

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,28 @@
 const ul = document.querySelector('.articleContainer');
 const listArticle = [
     {
-    title : 'hahahaha',
-    desc : 'lorem loremloreml oremloremloremloremloremloremloremloremloremloremloremloremloreml',
-    img : './assets/img/header.jpg'
+    title : 'JAPANNEST',
+    desc : 'Website ini merupakan sebuah website Landing Page sederhana yang saya buat dulu saat saya baru mencoba memasukin dunia Web Developer, landing page ini berbasis single page, jadi user hanya perlu meload website 1x saat pertama kali masuk kewebsite ini, teknologi yang saya gunakan hanya HTML, CSS, dan Javascript',
+    img : './assets/img/header.jpg',
+    link : 'https://japannest.glitch.me/'
     },
     {
-        title : 'hahahaha',
-        desc : 'lorem loremloreml oremloremloremloremloremloremloremloremloremloremloremloremloreml',
-        img : './assets/img/header.jpg'
+        title : 'Quranku',
+        desc : 'Quranku merupakan salah satu website yang saya buat dari hasil belajar saya, website ini merupakan sebuah website islami yang didalamnya terdapat sebuah content - content islami seperti  Al - Quran online, Kisah Nabi, Doa Sehari - Hari, Teknologi yang saya gunakan merupakan VueJs dan TailwindCSS untuk stylingnya.',
+        img : './assets/img/header.jpg',
+        link : 'https://alquranku.netlify.app/'
     },
     {
-        title : 'hahahaha',
-        desc : 'lorem loremloreml oremloremloremloremloremloremloremloremloremloremloremloremloreml',
-        img : './assets/img/header.jpg'
+        title : 'Guess Card Game',
+        desc : 'Website yang satu ini juga merupakan website buatan saya, yang dulu saya buat dari hasil belajar saya, website ini merupakan sebuah website menebak kartu sederhana yang dimana kita harus menebak kartu yang diminta dan diberikan kesempatan yang terbatas, Teknologi yang saya gunakan merupakan ReactJS dan TailwindCSS untuk stylingnya',
+        img : './assets/img/header.jpg',
+        link : 'https://kyocardgame.netlify.app/'
+    },
+    {
+        title : 'Grouping it !',
+        desc : 'Grouping it merupakan salah satu website saya juga, yang saya buat beberapa wakt yang lalu dari hasil belajar saya juga, website tersebut merupakan sebuah website berbasis SPA (Single Page App.) yang memungkinkan kita mengelompokan beberapa individu secara acak dan dapat menentukan ketua dari setiap kelompok yang dibuat.',
+        img : './assets/img/header.jpg',
+        link : 'https://kyocardgame.netlify.app/'
     }
 ]
 
@@ -22,13 +31,17 @@ function appendList () {
         let liEl = document.createElement("li");
         let h3 = document.createElement('h3');
         let h4 = document.createElement('h4');
+        let anchorEl = document.createElement('a')
         let divEl = document.createElement('div');
         let img = document.createElement('img');
-        img.setAttribute('src', `${lst.img}`)
+        img.setAttribute('src', `${lst.img}`);
+        anchorEl.setAttribute('href', `${lst.link}`)
+        anchorEl.textContent = 'Click Disini !'
         h3.textContent = lst.title
         h4.textContent = lst.desc
         divEl.appendChild(h3);
         divEl.appendChild(h4);
+        divEl.appendChild(anchorEl);
         liEl.appendChild(img);
         liEl.appendChild(divEl);
         
